d8d6bf7c70c793d75a03426418c8bc1bc6047f28
Jocelyn Berenice Zamora De La Cerda <201G0293@rcarbonifera.tecnm.mx>
2024-03-21 17:38:37 -0600

    mas cambios

--- MensajesServer/Services/DiscoveryServise.cs
+++ MensajesServer/Services/DiscoveryServise.cs
@@ -20,14 +20,34 @@ namespace MensajesServer.Services
         {
             var usuario = Environment.UserName;
             buffer = Encoding.UTF8.GetBytes(usuario);
+            Saludar();//Cuando saludamos en la red
+            new Thread(ResivirSaludo) { IsBackground = true }.Start();//esperar aque nos saluden
+            new Thread(StillAlive) { IsBackground = true }.Start();//para informar que seguimos vivos 
         }
         public void Saludar() 
         {
             server.Send(buffer,buffer.Length,destino);
         }
-        private void ResivirMensaje() 
+
+        private void StillAlive() 
         {
-            Saludar();
+            while (true) 
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(30));
+                Saludar();
+            }
+        }
+
+        private void ResivirSaludo() //Respondo el saludo cuando me saludan
+        {
+            UdpClient udp2 = new UdpClient(7001);
+            while (true) 
+            {
+                IPEndPoint remorto = new(IPAddress.Any, 0);//ve quien lo envio IP
+                byte[] buffer = udp2.Receive(ref remorto); 
+
+                server.Send(buffer,buffer.Length,remorto);
+            }
         }
     }
 }
--- MensajesServer/Services/MensajesService.cs
+++ MensajesServer/Services/MensajesService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,23 @@ namespace MensajesServer.Services
 {
     public class MensajesService
     {
+        HttpListener servidor = new();
+        public MensajesService()
+        {
+            servidor.Prefixes.Add("http://*:7002/mensajitos/");
+            servidor.Start();
+        }
+        void ResivirPeticiones()
+        {
+            while (true) 
+            {
+                var context = servidor.GetContext();
+                if (context != null) 
+                {
+                    if (context.Request.QueryString["mensaje"] != null) //si mandaron un mensaje que querystring
+                    { }
+                }
+            }
+        }
     }
 }
